c85aab857a49477ccd318936ec6c80187a45b8b4
orestis faltakas <ofaltakas@codezerolabs.com>
2022-06-06 19:28:31 +0300

    + take image from camera and score

--- app/src/main/java/com/userfaltakas/tensorflowdemo/MainActivity.kt
+++ app/src/main/java/com/userfaltakas/tensorflowdemo/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.userfaltakas.tensorflowdemo
 
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.userfaltakas.tensorflowdemo.databinding.ActivityMainBinding
 import com.userfaltakas.tensorflowdemo.ml.BirdsModel
 import org.tensorflow.lite.support.image.TensorImage
@@ -12,14 +18,41 @@ import org.tensorflow.lite.support.image.TensorImage
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val GALLERY_REQUEST_CODE = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val image = BitmapFactory.decodeResource(resources, R.drawable.bird)
-        outputGenerator(image)
+
+        binding.takeImageBtn.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                takePicturePreview.launch(null)
+            } else {
+                requestPermission.launch(android.Manifest.permission.CAMERA)
+            }
+        }
     }
+
+    //request camera permission
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (granted) {
+                takePicturePreview.launch(null)
+            } else {
+                Toast.makeText(this, "Permission Denied !! Try again", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private val takePicturePreview =
+        registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+            if (bitmap != null) {
+                binding.imageView.setImageBitmap(bitmap)
+                outputGenerator(bitmap)
+            }
+        }
 
 
     private fun outputGenerator(bitmap: Bitmap) {
@@ -32,6 +65,27 @@ class MainActivity : AppCompatActivity() {
         result.forEach {
             Log.d("bird category", it.toString())
         }
+        binding.resultTv.text = "${result.first().label} \n ${result.first().score}"
         model.close()
     }
+
+    private fun onResultReceived(requestCode: Int, result: ActivityResult?) {
+        when (requestCode) {
+            GALLERY_REQUEST_CODE -> {
+                if (result?.resultCode == Activity.RESULT_OK) {
+                    result.data?.data?.let { uri ->
+                        Log.i("TAG", "onResultReceived: $uri")
+                        val bitmap =
+                            BitmapFactory.decodeStream(contentResolver.openInputStream(uri))
+                        binding.imageView.setImageBitmap(bitmap)
+                        outputGenerator(bitmap)
+                    }
+                } else {
+                    Log.e("TAG", "onActivityResult: error in selecting image")
+                }
+            }
+        }
+    }
+
+
 }
